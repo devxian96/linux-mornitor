94412a409aa43cc0635a667ff79ef1b463cd6f79
DevXian <coolman555@me.com>
2021-10-23 17:15:28 +0900

    feat: 배경패턴 추가

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,18 +2,25 @@ import React, { useMemo } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { CssBaseline, useMediaQuery } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Particles from "react-particles-js";
+import styles from "./App.module.css";
 import Header from "../components/Header";
 import Home from "./Home";
 
 const App = () => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-
   // Dark Mode 테마 설정
   const theme = useMemo(
     () =>
       createTheme({
         palette: {
           mode: prefersDarkMode ? "dark" : "light",
+          primary: {
+            main: "#3B82EA",
+          },
+          secondary: {
+            main: "#05121B",
+          },
         },
       }),
     [prefersDarkMode]
@@ -22,6 +29,28 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Particles
+        className={styles.background}
+        params={{
+          color: "#75A5B7",
+          maxParticles: 130,
+          connectParticles: true,
+          responsive: [
+            {
+              breakpoint: 768,
+              options: {
+                maxParticles: 80,
+              },
+            },
+            {
+              breakpoint: 375,
+              options: {
+                maxParticles: 50,
+              },
+            },
+          ],
+        }}
+      />
       <Router>
         <Header />
         <Switch>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,10 @@
+import React, { useEffect } from "react";
+
 const Home = () => {
+  useEffect(() => {
+    document.title = "리눅스 모니터";
+  }, []);
+
   return <div>hi</div>;
 };
 
