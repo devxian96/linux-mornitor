7a0afc6c2c896967c233435e94a7f283926cc961
DevXian <coolman555@me.com>
2021-10-31 19:05:30 +0900

    test

--- build/api/index.php
+++ build/api/index.php
@@ -4,7 +4,7 @@ require './phpExpress.php';
 $app = new phpExpress();
 
 // 시스템 정보 전송
-$app->get('/system', function ($req, $res) {
+$app->get('/', function ($req, $res) {
     try {
         $res->send("hi");
     } catch (Exception $e) {
