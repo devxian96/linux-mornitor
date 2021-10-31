0bbc65cba607e0fd352f407fda3a6065f0d020aa
DevXian <coolman555@me.com>
2021-10-31 19:02:42 +0900

    test: test code

---
+++ public/api/index.php
@@ -0,0 +1,14 @@
+<?php
+require './phpExpress.php';
+
+$app = new phpExpress();
+
+// 시스템 정보 전송
+$app->get('/systemInfo', function ($req, $res) {
+    try {
+        $res->send("hi");
+    } catch (Exception $e) {
+        http_response_code(500);
+        $res->send($e);
+    }
+});
